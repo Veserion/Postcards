91b2b322f416208537716806e2ff1bfce30b1d16
Veserion <48389713+Veserion@users.noreply.github.com>
2020-03-26 21:10:14 +0300

    fixed stiles

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,17 +3,33 @@ import styled from '@emotion/styled'
 import BigButton from '../BigButton'
 import Title from '../Title'
 
+const Item = styled.div`
+width: 200px;
+height: 200px;
+background: tomato;
+`
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: -20px;
+  & > * {
+    margin: 20px;
+  }
+`
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return <Root>
-      <BigButton/>
-      <Title/>
+      <BigButton />
+      <Title />
+      <ItemWrapper>{Array.from({ length: 23 }, (_, i) => <Item key={i} />)}</ItemWrapper>
     </Root>
   }
 }
 
-const Root = styled.div `
+const Root = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: flex-start;
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,23 +5,22 @@ import { css, jsx } from '@emotion/core'
 
 export default class Title extends React.Component {
     render() {
-        return <Root>:Doodle Patterns 
+        return <Root>:Doodle Patterns
                 <SubTitle>Collection of 61 seamless hand-drawn vector patterns crafted to make your next project shine ;)</SubTitle>
-            </Root>
+        </Root>
     }
 }
 
 const Root = styled.div`
-width: 864px;
-height: 120px;
 margin-top: 200px;
 font-family: 'Gaegu', cursive;
 margin-bottom: 12px;
 font-size: 120px;
+text-align: center;
 `
 const SubTitle = styled.div`
-width: 100%;
-height: auto;
+height: 100%;
 font-size: 40px;
 line-height: 50px;
+margin: 0 100px;
 `
