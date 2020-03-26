deb9ccb1c214c054022470010d5fc30a2c9dc2aa
Veserion <48389713+Veserion@users.noreply.github.com>
2020-03-26 22:19:52 +0300

    fixed stiles :rocket:

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,29 +2,15 @@ import React from 'react'
 import styled from '@emotion/styled'
 import BigButton from '../BigButton'
 import Title from '../Title'
+import Items from '../Items'
 
-const Item = styled.div`
-width: 200px;
-height: 200px;
-background: tomato;
-`
-
-const ItemWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: -20px;
-  & > * {
-    margin: 20px;
-  }
-`
 
 export default class App extends React.Component {
   render() {
     return <Root>
       <BigButton />
       <Title />
-      <ItemWrapper>{Array.from({ length: 23 }, (_, i) => <Item key={i} />)}</ItemWrapper>
+      <Items/>
     </Root>
   }
 }
--- src/components/BigButton/index.tsx
+++ src/components/BigButton/index.tsx
@@ -7,12 +7,13 @@ import bgWithFocus from '../../icons/bigbutton/withFocus.png'
 export default class BigButton extends React.Component {
     render() {
         return <Root>
-            <Text>Grab all for <b css={css`font-weight: bold`}>$35</b></Text>
+            <Text>Grab all for <b>$35</b></Text>
         </Root>
     }
 }
 
 const Root = styled.div`
+/* transition: .5s; */
 position: fixed;
 top: 5%;
 right: 5%;
@@ -22,10 +23,10 @@ align-items: center;
 width: 245px;
 height: 72px;
 padding: 16px 24px;
-background-image: url(${bgWithoutFocus});
+background-image: url(${bgWithFocus});
 background-size: 100% 100%;
 :hover {
-    background-image: url(${bgWithFocus});
+    background-image: url(${bgWithoutFocus});
 }
 `
 const Text = styled.div`
@@ -33,4 +34,5 @@ width: auto;
 height: auto;
 font-family: 'Gaegu', cursive;
 font-size: 28px;
+b {font-weight: bold;}
 `
