0e325a068118fb5ee4d25055b7ad02a1184f54b3
Алешенька <alexey@Alesenkas-MacBook-Pro.local>
2020-03-27 00:39:26 +0300

    Updates

---
+++ precache-manifest.979998429e7fdfc9897b19ac8c877773.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d74b1befd63588f86f8f620e87fc7b",
+    "url": "/postcards/index.html"
+  },
+  {
+    "revision": "3bccc4b9b23a988f07bb",
+    "url": "/postcards/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3bccc4b9b23a988f07bb",
+    "url": "/postcards/static/js/2.d579e26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/postcards/static/js/2.d579e26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd994607c0d3b3f2a4d",
+    "url": "/postcards/static/js/main.07f5bf2b.chunk.js"
+  },
+  {
+    "revision": "414462ea341b245ae53a",
+    "url": "/postcards/static/js/runtime-main.bb0756b9.js"
+  },
+  {
+    "revision": "b1e9a04b9bb7b35f3ad83888f82852f5",
+    "url": "/postcards/static/media/card1.b1e9a04b.jpg"
+  },
+  {
+    "revision": "d360142980452ab468bb2f49f5269ff5",
+    "url": "/postcards/static/media/card10.d3601429.jpg"
+  },
+  {
+    "revision": "4414cc37c3963f035084390baa4e6ec5",
+    "url": "/postcards/static/media/card11.4414cc37.jpg"
+  },
+  {
+    "revision": "2fcf1f1606dbf81a0d17cf96106502cb",
+    "url": "/postcards/static/media/card12.2fcf1f16.jpg"
+  },
+  {
+    "revision": "1b8d27950d2bbc818ac7f367bd13a207",
+    "url": "/postcards/static/media/card13.1b8d2795.jpg"
+  },
+  {
+    "revision": "5f47f8a168496b562fae87d45f55f420",
+    "url": "/postcards/static/media/card14.5f47f8a1.jpg"
+  },
+  {
+    "revision": "e82bcd7a57f4a4506a27eea66934ac9c",
+    "url": "/postcards/static/media/card15.e82bcd7a.jpg"
+  },
+  {
+    "revision": "6001512713f45030847955b6c495cb82",
+    "url": "/postcards/static/media/card17.60015127.jpg"
+  },
+  {
+    "revision": "d162a2ecca4fa982dfca8bc4d8b460b7",
+    "url": "/postcards/static/media/card18.d162a2ec.jpg"
+  },
+  {
+    "revision": "cb19a782ab1907119e33ff96fbd529e4",
+    "url": "/postcards/static/media/card19.cb19a782.jpg"
+  },
+  {
+    "revision": "71905a9a2511ea92432f1eff4ad351b6",
+    "url": "/postcards/static/media/card2.71905a9a.jpg"
+  },
+  {
+    "revision": "b91a31f33aa77fb44a93aa373ed10aeb",
+    "url": "/postcards/static/media/card20.b91a31f3.jpg"
+  },
+  {
+    "revision": "3bdf136d4fff3ff4539f70f806a3e692",
+    "url": "/postcards/static/media/card21.3bdf136d.jpg"
+  },
+  {
+    "revision": "545b9ebc85e5fecf314f6e42622312d7",
+    "url": "/postcards/static/media/card22.545b9ebc.jpg"
+  },
+  {
+    "revision": "3a403b7fdcd0a8b1f12b1626cd43cc2f",
+    "url": "/postcards/static/media/card23.3a403b7f.jpg"
+  },
+  {
+    "revision": "6b448696cb04c0e99cda2e61bcd78668",
+    "url": "/postcards/static/media/card24.6b448696.jpg"
+  },
+  {
+    "revision": "800fd95aab17ba88be21cc63f15164fd",
+    "url": "/postcards/static/media/card25.800fd95a.jpg"
+  },
+  {
+    "revision": "89f090a24686c6d26dc07935dffafa2b",
+    "url": "/postcards/static/media/card26.89f090a2.jpg"
+  },
+  {
+    "revision": "2be68ed56b032b9d93c46961a097c84d",
+    "url": "/postcards/static/media/card27.2be68ed5.jpg"
+  },
+  {
+    "revision": "8ccfb0209887c50758faed160330350a",
+    "url": "/postcards/static/media/card28.8ccfb020.jpg"
+  },
+  {
+    "revision": "85ed73c908c3dacffcd8f1879c49462d",
+    "url": "/postcards/static/media/card29.85ed73c9.jpg"
+  },
+  {
+    "revision": "dae43631f547bd2686bca22774b15b79",
+    "url": "/postcards/static/media/card3.dae43631.jpg"
+  },
+  {
+    "revision": "569a96259a10d7c7602d192017ae91ec",
+    "url": "/postcards/static/media/card30.569a9625.jpg"
+  },
+  {
+    "revision": "0570723a8bf26b1c946f573c305cb87b",
+    "url": "/postcards/static/media/card31.0570723a.jpg"
+  },
+  {
+    "revision": "a348b8a5356a8b8ca1b3321f3bfde84f",
+    "url": "/postcards/static/media/card32.a348b8a5.jpg"
+  },
+  {
+    "revision": "241175426f3f00597a810278ae8143f3",
+    "url": "/postcards/static/media/card33.24117542.jpg"
+  },
+  {
+    "revision": "595ca12e8ada3fc420d69249c14d1226",
+    "url": "/postcards/static/media/card34.595ca12e.jpg"
+  },
+  {
+    "revision": "0054a54076d1427ed7277bceef0aff26",
+    "url": "/postcards/static/media/card35.0054a540.jpg"
+  },
+  {
+    "revision": "19af4c84385d15d81e67cfc7b525c140",
+    "url": "/postcards/static/media/card36.19af4c84.jpg"
+  },
+  {
+    "revision": "9887cd99e7e7ea5d45ed598a71fa3004",
+    "url": "/postcards/static/media/card37.9887cd99.jpg"
+  },
+  {
+    "revision": "cb16cb1171f7c3c78de7bf54556ce270",
+    "url": "/postcards/static/media/card38.cb16cb11.jpg"
+  },
+  {
+    "revision": "d9a0ff4f00f908ca37829e7d859eb7c6",
+    "url": "/postcards/static/media/card4.d9a0ff4f.jpg"
+  },
+  {
+    "revision": "5fc732626cb51f62e9330bd66582cedb",
+    "url": "/postcards/static/media/card40.5fc73262.jpg"
+  },
+  {
+    "revision": "55ca64891e8a56dcd14b92d7fb2546bd",
+    "url": "/postcards/static/media/card41.55ca6489.jpg"
+  },
+  {
+    "revision": "18cc649a2deb1ffb15dca650e4c3678d",
+    "url": "/postcards/static/media/card42.18cc649a.jpg"
+  },
+  {
+    "revision": "a9154a298a8077f8e43a1c944f60d70a",
+    "url": "/postcards/static/media/card43.a9154a29.jpg"
+  },
+  {
+    "revision": "a9b4120c470d445434d3d0e2dbf61e62",
+    "url": "/postcards/static/media/card44.a9b4120c.jpg"
+  },
+  {
+    "revision": "160d71d5f14f82fe2781aacb48d9a138",
+    "url": "/postcards/static/media/card45.160d71d5.jpg"
+  },
+  {
+    "revision": "20a0b218486493e450490a3188afef4b",
+    "url": "/postcards/static/media/card46.20a0b218.jpg"
+  },
+  {
+    "revision": "f430d1d1838ab38e95563d34fcbb5a29",
+    "url": "/postcards/static/media/card47.f430d1d1.jpg"
+  },
+  {
+    "revision": "6487e13a2be2fd002799c444565059cc",
+    "url": "/postcards/static/media/card48.6487e13a.jpg"
+  },
+  {
+    "revision": "439afc15fb81ee1dd52b62ac175afe55",
+    "url": "/postcards/static/media/card49.439afc15.jpg"
+  },
+  {
+    "revision": "a09437fe83a9614dfcd900d3d8d89e9f",
+    "url": "/postcards/static/media/card5.a09437fe.jpg"
+  },
+  {
+    "revision": "dc2ea8fa17950e10622036c1be32b823",
+    "url": "/postcards/static/media/card50.dc2ea8fa.jpg"
+  },
+  {
+    "revision": "05f799079999427b55345eb9bba66dd8",
+    "url": "/postcards/static/media/card51.05f79907.jpg"
+  },
+  {
+    "revision": "7dd8f54646c18349ac2c2f05e8637627",
+    "url": "/postcards/static/media/card52.7dd8f546.jpg"
+  },
+  {
+    "revision": "c3a810b4938df372005bbd18931fe3da",
+    "url": "/postcards/static/media/card53.c3a810b4.jpg"
+  },
+  {
+    "revision": "7ccb20181319a84f39e9c71bf73a4119",
+    "url": "/postcards/static/media/card54.7ccb2018.jpg"
+  },
+  {
+    "revision": "f5a33773ea4bf18401d652eb5dbd361b",
+    "url": "/postcards/static/media/card55.f5a33773.jpg"
+  },
+  {
+    "revision": "3f892435601e314d6df1d0182b35f783",
+    "url": "/postcards/static/media/card56.3f892435.jpg"
+  },
+  {
+    "revision": "878afa281643da4a237096c09cdc3c82",
+    "url": "/postcards/static/media/card57.878afa28.jpg"
+  },
+  {
+    "revision": "df5ea1f0b7bbf32af893b04f16beee1e",
+    "url": "/postcards/static/media/card58.df5ea1f0.jpg"
+  },
+  {
+    "revision": "099f4bf0fede90448b6a1bc725edfa07",
+    "url": "/postcards/static/media/card6.099f4bf0.jpg"
+  },
+  {
+    "revision": "efad7a5e0e82335fad268047424f07f5",
+    "url": "/postcards/static/media/card60.efad7a5e.jpg"
+  },
+  {
+    "revision": "0c3083cd178a8d6e824044a41ea57eae",
+    "url": "/postcards/static/media/card61.0c3083cd.jpg"
+  },
+  {
+    "revision": "96ed48a0f9ded98d882447e1a3acb549",
+    "url": "/postcards/static/media/card62.96ed48a0.jpg"
+  },
+  {
+    "revision": "5beefed9d904ba17101dff86804ded62",
+    "url": "/postcards/static/media/card63.5beefed9.jpg"
+  },
+  {
+    "revision": "0a23a0f7dc3a4a611d6c3624cf6b43e2",
+    "url": "/postcards/static/media/card64.0a23a0f7.jpg"
+  },
+  {
+    "revision": "0b507c312e81a3e821f61dada1ccfb76",
+    "url": "/postcards/static/media/card65.0b507c31.jpg"
+  },
+  {
+    "revision": "0063483737fab1e305da31e8970e0801",
+    "url": "/postcards/static/media/card66.00634837.jpg"
+  },
+  {
+    "revision": "debeef0a70f983a9ce045bd51b479e2e",
+    "url": "/postcards/static/media/card67.debeef0a.jpg"
+  },
+  {
+    "revision": "c36cf682d66125521a132cc20d843ab5",
+    "url": "/postcards/static/media/card68.c36cf682.jpg"
+  },
+  {
+    "revision": "a44942aa42fa48ba4801f4215d876876",
+    "url": "/postcards/static/media/card69.a44942aa.jpg"
+  },
+  {
+    "revision": "4e67186486b2d180e27399e6cbd80fe1",
+    "url": "/postcards/static/media/card7.4e671864.jpg"
+  },
+  {
+    "revision": "111dccb59943acf91fb322d07fe270ed",
+    "url": "/postcards/static/media/card70.111dccb5.jpg"
+  },
+  {
+    "revision": "d6f29aeb31e2a1b44f5154add20677a9",
+    "url": "/postcards/static/media/card71.d6f29aeb.jpg"
+  },
+  {
+    "revision": "61ad0b72230776e4d0dbbac01089eae4",
+    "url": "/postcards/static/media/card72.61ad0b72.jpg"
+  },
+  {
+    "revision": "c6fbaa6d2f1a54eeb4beea433131c538",
+    "url": "/postcards/static/media/card73.c6fbaa6d.jpg"
+  },
+  {
+    "revision": "08e6c58b1f477f0f5a1ae156e18893f9",
+    "url": "/postcards/static/media/card74.08e6c58b.jpg"
+  },
+  {
+    "revision": "28a3d7b695a3e6aed3ce7a26362ab876",
+    "url": "/postcards/static/media/card75.28a3d7b6.jpg"
+  },
+  {
+    "revision": "48d633a2adb2cdf743b8efd34787a7d5",
+    "url": "/postcards/static/media/card8.48d633a2.jpg"
+  },
+  {
+    "revision": "202ff6f82015f1033792666f839c4768",
+    "url": "/postcards/static/media/card9.202ff6f8.jpg"
+  }
+]);
